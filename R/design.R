fd4fb746699dbc2650123f5871b782177471b8af
ielbadisy <elbadisyimad@gmail.com>
2026-08-20 14:26:14 +0100

    refactor: rename tvc to tve (time-varying effect) in the design builder

--- R/design.R
+++ R/design.R
@@ -2,10 +2,10 @@
 #'
 #' Constructs `eta`'s design matrix `X` and the design matrix for
 #' `d(eta)/d(log t)`, `dX`, which share the same column layout:
-#' `[intercept, baseline spline(log t), covariates, tvc spline(log t):covariate]`.
+#' `[intercept, baseline spline(log t), covariates, tve spline(log t):covariate]`.
 #'
 #' @keywords internal
-rp_design <- function(log_time, cov_data, knots, tvc = NULL, tvc_knots = NULL) {
+rp_design <- function(log_time, cov_data, knots, tve = NULL, tve_knots = NULL) {
   p_base <- ncol(rcs_basis(log_time, knots))
   X_base <- cbind(1, rcs_basis(log_time, knots))
   dX_base <- cbind(0, rcs_basis(log_time, knots, derivative = TRUE))
@@ -21,27 +21,27 @@ rp_design <- function(log_time, cov_data, knots, tvc = NULL, tvc_knots = NULL) {
     dX_cov <- X_cov
   }
 
-  X_tvc <- NULL
-  dX_tvc <- NULL
-  if (!is.null(tvc) && length(tvc)) {
-    X_tvc_list <- vector("list", length(tvc))
-    dX_tvc_list <- vector("list", length(tvc))
-    for (i in seq_along(tvc)) {
-      var <- tvc[i]
-      k <- tvc_knots[[var]]
+  X_tve <- NULL
+  dX_tve <- NULL
+  if (!is.null(tve) && length(tve)) {
+    X_tve_list <- vector("list", length(tve))
+    dX_tve_list <- vector("list", length(tve))
+    for (i in seq_along(tve)) {
+      var <- tve[i]
+      k <- tve_knots[[var]]
       basis <- rcs_basis(log_time, k)
       dbasis <- rcs_basis(log_time, k, derivative = TRUE)
       x <- cov_data[[var]]
-      X_tvc_list[[i]] <- basis * x
-      dX_tvc_list[[i]] <- dbasis * x
-      colnames(X_tvc_list[[i]]) <- colnames(dX_tvc_list[[i]]) <-
+      X_tve_list[[i]] <- basis * x
+      dX_tve_list[[i]] <- dbasis * x
+      colnames(X_tve_list[[i]]) <- colnames(dX_tve_list[[i]]) <-
         paste0(var, ":s(logt)", seq_len(ncol(basis)))
     }
-    X_tvc <- do.call(cbind, X_tvc_list)
-    dX_tvc <- do.call(cbind, dX_tvc_list)
+    X_tve <- do.call(cbind, X_tve_list)
+    dX_tve <- do.call(cbind, dX_tve_list)
   }
 
-  X <- cbind(X_base, X_cov, X_tvc)
-  dX <- cbind(dX_base, dX_cov, dX_tvc)
+  X <- cbind(X_base, X_cov, X_tve)
+  dX <- cbind(dX_base, dX_cov, dX_tve)
   list(X = X, dX = dX, p_base = p_base + 1L)
 }
